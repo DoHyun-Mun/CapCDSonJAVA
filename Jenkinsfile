7aa2a97f61ef5f505855e9fe72893dcaa6818ec9
DoHyun Moon <P000020>
2023-02-22 14:40:59 +0000

    init commit

---
+++ Jenkinsfile
@@ -0,0 +1,32 @@
+#!/usr/bin/env groovy 
+
+/*
+ * This file bootstraps the codified Continuous Delivery pipeline for extensions of SAP solutions such as SAP S/4HANA.
+ * The pipeline helps you to deliver software changes quickly and in a reliable manner.
+ * A suitable Jenkins instance is required to run the pipeline.
+ * More information on getting started with Continuous Delivery can be found here: https://sap.github.io/jenkins-library/
+ */
+
+//@Library('piper-lib-os') _
+//piperPipeline script: this
+
+@Library('piper-lib-os') _
+
+node() {
+    stage('prepare') {
+        deleteDir()
+        checkout scm
+        setupCommonPipelineEnvironment script:this
+    }
+    stage('build') {
+        mtaBuild script: this
+    }
+    stage('deploy') {
+        cloudFoundryDeploy script: this
+    }
+    /*
+    stage('tmsupload') {
+        tmsUpload script: this
+    }
+    */
+}
---
+++ srv/src/test/java/customer/cdsmta_test/handlers/CatalogServiceHandlerTest.java
@@ -0,0 +1,42 @@
+package customer.cdsmta_test.handlers;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.stream.Stream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import cds.gen.catalogservice.Books;
+
+public class CatalogServiceHandlerTest {
+
+	private CatalogServiceHandler handler = new CatalogServiceHandler();
+	private Books book = Books.create();
+
+	@Before
+	public void prepareBook() {
+		book.setTitle("title");
+	}
+
+	@Test
+	public void testDiscount() {
+		book.setStock(500);
+		handler.discountBooks(Stream.of(book));
+		assertEquals("title (discounted)", book.getTitle());
+	}
+
+	@Test
+	public void testNoDiscount() {
+		book.setStock(100);
+		handler.discountBooks(Stream.of(book));
+		assertEquals("title", book.getTitle());
+	}
+
+	@Test
+	public void testNoStockAvailable() {
+		handler.discountBooks(Stream.of(book));
+		assertEquals("title", book.getTitle());
+	}
+
+}
